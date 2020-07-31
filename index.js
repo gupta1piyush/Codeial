240d81704351598d6d490b608d892badaff27265
Piyush Gupta <gupta1piyush@gmail.com>
2020-07-31 16:49:33 -0700

    third commit

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port=8900;
 
 app.use('/',require('./routes'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error:${err}`);
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router=express.Router();
 const homeController=require('../controller/home_controller');
 
 router.get('/',homeController.home);
+router.use('/user',require('./user'));
 
 module.exports=router;
