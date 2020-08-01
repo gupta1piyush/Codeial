aadc602adc20832c05fdff256307b1a1afb9d1cf
Piyush Gupta <gupta1piyush@gmail.com>
2020-08-01 00:21:48 -0700

    Sign up complete

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,3 +1,4 @@
+const User=require('../models/user');
 module.exports.profile=function(req,res){
     res.render('user_profile',{
         title:"Profile"
@@ -13,5 +14,23 @@ module.exports.signUp=function(req,res){
 module.exports.signIn=function(req,res){
     res.render('user_sign_in',{
         title:"Codeial:SignIn"
+    })
+}
+
+module.exports.create=function(req,res){
+    if(req.body.password!=req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('Error in finding up user'); return}
+
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){console.log('error in creating User');return}
+                return res.redirect('/user/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
     })
 }
