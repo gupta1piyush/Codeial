d2cf94657cb18997d36beebe044f6cb6f51b8ec1
Piyush Gupta <gupta1piyush@gmail.com>
2020-08-03 08:53:03 -0700

    Profile page shown on signin other wise not

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -48,7 +48,20 @@ passport.deserializeUser(function(id, done){
         return done(null, user);
     });
 });
+passport.checkAuthentication=function(req,res,next){
+    //if the user is signed in then pass on request to the next function
+    if(req.isAuthenticated()){
+        return next();
+    }
 
+    //if the user is not signed in
+    return res.redirect('/user/sign-in');
+}
 
-
+passport.setAuthenticatedUser=function(req,res,next){
+    if(req.isAuthenticated()){
+        res.locals.user=req.user;
+    }
+    next();
+}
 module.exports = passport;
