31cbf27e9c4fc5ff0f0a1f4c72e1c75dd87bd36e
Piyush Gupta <gupta1piyush@gmail.com>
2020-08-03 09:25:38 -0700

    cookie stored in mongo for further use

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const db=require('./config/mongoose');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
-
+const MongoStore=require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -24,6 +24,7 @@ app.set('layout extractScripts',true);
 app.set('view engine','ejs');
 app.set('views','./views');
 
+//mongo store is used to store the session cookie in the db
 app.use(session({
     name:'codeial',
     secret:'blah sometihing',
@@ -31,7 +32,16 @@ app.use(session({
     resave:false,
     cookie:{
         maxAge:(1000*60*100)
-    }
+    },
+    store:new MongoStore(
+        {
+            mongooseConnection:db,
+            autoRemove:'disabled'
+        },
+        function(err){
+            console.log(err||'connect-mongodb  setup ok');
+        }
+    )
 }));
 app.use(passport.initialize());
 app.use(passport.session());
