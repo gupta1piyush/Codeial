c4e3b2b82f54128223a0a17f61c4b36491f2f244
Piyush Gupta <gupta1piyush@gmail.com>
2020-08-05 16:57:06 -0700

    Comments Send to database and loaded in posts too

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const router=express.Router();
 const homeController=require('../controller/home_controller');
+const { route } = require('./comments');
 
 router.get('/',homeController.home);
 router.use('/user',require('./user'));
 router.use('/posts',require('./posts'));
+router.use('/comments',require('./comments'));
 
 module.exports=router;
