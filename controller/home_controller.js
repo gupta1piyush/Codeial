2b9aad10fe286f9ef7d1d746a4b731253a4e68e7
Piyush Gupta <gupta1piyush@gmail.com>
2020-08-05 15:07:52 -0700

    Posts visible with names

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,11 +1,15 @@
-const Post =require('../models/post');
-const { post } = require('../routes');
-module.exports.home=function(req,res){
-    Post.find({},function(err,posts){
-        return res.render('home',{
-            title:"Codeial || Home",
-            posts:posts
+const Post = require('../models/post');
+
+module.exports.home = function(req, res){
+   
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
         });
-    });
-    
+    })
+
 }
+
--- models/post.js
+++ models/post.js
@@ -1,15 +1,18 @@
-const mongoose=require('mongoose');
-const postSchema=new mongoose.Schema({
-    content:{
-        type:String,
-        required:true
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
     },
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
+    user: {
+        type:  mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+
     }
 },{
-    timestamps:true
+    timestamps: true
 });
-const Post=mongoose.model('Post',postSchema);
-module.exports=Post;
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
