609955890ae359bdddd20458782fe4dfe03645e8
Piyush Gupta <gupta1piyush@gmail.com>
2020-08-06 10:50:49 -0700

    In home_controller populate was not looking cleaner cleaned it using async and await commands

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,27 +1,25 @@
 const Post = require('../models/post');
 const User=require('../models/user');
-module.exports.home = function(req, res){
-   
-
-    // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err, posts){
-        User.find({},function(err,users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:  posts,
-                all_users:users
-            });
-        })
-        
-    })
-
+module.exports.home = async function(req, res){
+    try{
+        //populate the user of each post
+        let posts =await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        });
+        let users= await User.find({});
+        return res.render('home',{
+            title:"Codeial||home",
+            posts:posts,
+            all_users:users
+        });
+    }catch(err){
+        console.log('error',err);
+        return;
+    }
 }
 
