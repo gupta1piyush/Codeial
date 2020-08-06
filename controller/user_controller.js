c1f73eb2373d2237de32fed7f24b8a0293851d9c
Piyush Gupta <gupta1piyush@gmail.com>
2020-08-05 23:27:29 -0700

    Displaying Friends profile

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,8 +1,12 @@
 const User=require('../models/user');
 module.exports.profile=function(req,res){
-    res.render('user_profile',{
-        title:"Profile"
+    User.findById(req.params.id,function(err,user){
+        res.render('user_profile',{
+            title:"Profile",
+            profile_user:user
+        });
     });
+    
 }
 
 module.exports.signUp=function(req,res){
