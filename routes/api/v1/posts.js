41ad06e88b845532a0685dc83f691deb276202ca
Piyush Gupta <gupta1piyush@gmail.com>
2020-08-24 22:19:53 -0700

    Setting up passport JWT

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -3,5 +3,6 @@ const router=express.Router();
 const postsApi=require("../../../controller/api/v1/post_api");
 
 router.get('/',postsApi.index);
+router.delete('/:id',postsApi.destroy);
 
 module.exports=router;
