de5954dc59af02f469f8a1e9d5b53764cd2ffa8e
Piyush Gupta <gupta1piyush@gmail.com>
2020-08-28 16:15:26 -0700

    We are now able to send the mails

---
+++ mailers/comments_mailer.js
@@ -0,0 +1,19 @@
+const nodeMailer=require('../config/nodemailer');
+
+
+exports.newComment=(comment)=>{
+    let htmlString=nodeMailer.renderTemplate({comment:comment},'/comments/new_comment.ejs');
+    nodeMailer.transporter.sendMail({
+        from:'gupta1piyush@gmail.com',
+        to:comment.user.email,
+        subject:"New comment Published!",
+        html:htmlString
+    },(err,info)=>{
+        if(err){
+            console.log('Error in sending emails',err);
+            return;
+        }
+        console.log("message sent",info);
+        return;
+    });
+}
