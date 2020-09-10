66108ce78f95a48be713ceb2e1a448452887bff9
Piyush Gupta <gupta1piyush@gmail.com>
2020-09-10 15:27:43 -0700

    Chat engine done

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,5 +1,24 @@
 //back end or server side that is going to interact with your front end chatting engine
 
-module.exports.chatSockets=function(){
+module.exports.chatSockets=function(socketServer){
+    let io=require('socket.io')(socketServer);
 
+    io.sockets.on('connection',function(socket){
+        console.log('new connection received',socket.id);
+
+        socket.on('disconnect',function(){
+            console.log('Socket disconnected');
+        });
+
+        socket.on('join_room',function(data){
+            console.log('joining req received',data);
+
+            socket.join(data.chatroom);
+            io.in(data.chatroom).emit('user_joined',data);
+        });
+        socket.on('send_message', function(data){
+            io.in(data.chatroom).emit('receive_message', data);
+        });
+
+    });
 }
