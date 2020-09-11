56e72350dbd136e59ed0e46974f6dc4310d085fc
Piyush Gupta <gupta1piyush@gmail.com>
2020-09-11 14:53:25 -0700

    Environment variables setup done

--- config/environment.js
+++ config/environment.js
@@ -22,6 +22,24 @@ const development={
 
 const production={
     name:'production',
+    asset_path:process.env.CODEIAL_ASSET_PATH,
+    session_cookie_key:process.env.CODEIAL_SESSION_COOKIE,
+    db:process.env.CODEIAL_DATABASE,
+    smtp:{
+        service:'gmail',
+        host:'smtp.gmail.com',
+        port:587,
+        secure:false,
+        auth:{
+            user:process.env.CODEIAL_GMAIL_USERNAME,
+            pass:process.env.CODEIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_id:process.env.CODEIAL_CLIENT_ID,
+    google_client_secret:process.env.CODEIAL_CLIENT_SECRET,
+    google_call_back_url: process.env.CODEIAL_CALL_BACK_URL,
+    jwt_secret:process.env.CODEIAL_JWT,
+    
 }
 
 module.exports=development;
